ddcdb816750a6a9988f9242b3119366cb5e0a9f3
quin1251 <55711083+quin1251@users.noreply.github.com>
2019-10-25 14:25:21 -0400

    Update and rename v.ino to servo.ino

--- Knob/servo.ino
+++ Knob/servo.ino
@@ -1,15 +1,15 @@
 void servo1(){
   int delay1=random(400,1000);
-   myservo.write(100);
+   myservo.write(180);
   delay(delay1);
-   myservo.write(10);
+   myservo.write(0);
   delay (delay1);
 }
 
 void servo2(){
   int delay2=random(400,1000);
-   myservo2.write(100);
+   myservo2.write(180);
   delay(delay2);
-   myservo2.write(10);
+   myservo2.write(0);
   delay (delay2);
 }
